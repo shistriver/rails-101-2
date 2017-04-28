d2911670d56c7df4b1fdd0e6895bc2baed8c3719
shistriver <954917356@qq.com>
2017-04-28 13:50:07 +0800

    add edit/delete to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,31 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group =Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: 'Post Updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Post Deleted"
+    redirect_to account_posts_path
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
